5bb13eb83cfbafff35f6facf596e8422145d5fd1
toyondiamond <eplmpopova@mail.ru>
2022-10-27 00:51:20 +0300

    сделано дз часть1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        System.out.println("Task1");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+        //task2
+        System.out.println("Task2");
+        for (int i = 10; i >=1 ; i--) {
+            System.out.println(i);
+        }
+        //task3
+        System.out.println("Task3");
+        for (int i = 0; i < 17; i=i+2) {
+            System.out.println(i);
+        }
+        //task4
+        System.out.println("Task4");
+        for (int i = 10; i >=-10; i--) {
+            System.out.println(i);
+        }
+    }
+}
